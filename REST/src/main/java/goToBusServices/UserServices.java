02284202a866b44b688bd1f623458c7fc7be398c
m-hesham01 <mohamedhesham2001@icloud.com>
2022-05-30 22:07:19 +0200

    applied entity manager to the UserServices Class

--- REST/src/main/java/goToBusServices/UserServices.java
+++ REST/src/main/java/goToBusServices/UserServices.java
@@ -2,6 +2,8 @@ package goToBusServices;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -11,24 +13,30 @@ import javax.ws.rs.core.MediaType;
 import entities.User;
 
 
-
 @EJB
 @Stateful
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserServices {
 
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	
 	@Path("/user")
 	@POST
-	public void createUser() {
-		User user = new User();
+	public void createUser(User u) {
+		entityManager.persist(u);
 	}
 	
 	
 	@Path("/login")
 	@POST
-	public void Login() {
-		
+	public String login(String username, String password) {
+		User user = entityManager.find(User.class, username);
+		if (password.equals(user.getPassword())) {
+			return "OK";
+		}
+		else {return "login failed";}
 	}
 }
